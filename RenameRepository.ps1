f7edf761d65522471ee793b697262a16006624fd
Pipeline <pipeline@demo.com>
2022-11-28 13:55:51 -0600

    Cleanup

--- RenameRepository.ps1
+++
@@ -1,5 +0,0 @@
-az acr repository show --name lllbicepregistry --repository bicep/cosmosdatabase
-az acr repository update -n lllbicepregistry --image bicep/cosmosdatabase:2022-08-24.256 --write-enabled false
-az acr repository untag -n lllbicepregistry --image bicep/cosmosdatabase:2022-08-24.256 
-
-az acr import --name lllbicepregistry --source lllbicepregistry.azurecr.io/bicep/cosmosdatabase:2022-08-24.256 --image bicep/cosmosdatabase:latest --force
