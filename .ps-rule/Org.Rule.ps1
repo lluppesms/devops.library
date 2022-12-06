8ae9b37e6eebeed1ca0a4c8acd8a9f523d25183c
Pipeline <pipeline@demo.com>
2022-12-06 10:53:10 -0600

    Update psrule pieces

--- .ps-rule/Org.Rule.ps1
+++ .ps-rule/Org.Rule.ps1
@@ -1,12 +1,12 @@
-# # Copyright (c) Microsoft Corporation.
-# # Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
-# # Note:
-# # This script demonstrates using PowerShell-based rules.
+# Note:
+# This script demonstrates using PowerShell-based rules.
 
-# # Synopsis: Policy exemptions must be approved by the security team and stored within deployments/contoso/landing-zones/subscription-1/policy/.
-# Rule 'Org.CodeOwners' -Type 'Microsoft.Authorization/policyExemptions' {
-#     $Assert.WithinPath($PSRule.Source['Parameter'], 'File', @(
-#         'template/deployments/contoso/landing-zones/subscription-1/policy/'
-#     ));
-# }
+# Synopsis: Policy exemptions must be approved by the security team and stored within deployments/contoso/landing-zones/subscription-1/policy/.
+Rule 'Org.CodeOwners' -Type 'Microsoft.Authorization/policyExemptions' {
+    $Assert.WithinPath($PSRule.Source['Parameter'], 'File', @(
+        'template/deployments/contoso/landing-zones/subscription-1/policy/'
+    ));
+}
